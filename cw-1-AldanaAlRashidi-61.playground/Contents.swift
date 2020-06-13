a174f1f4f5e06c701cb0b0f3f4db250c11016b75
61-الدانة سعد الرشيدي <aldanaalrashidi0@gmail.com>
2020-06-13 20:24:14 +0300

    cw-1 done

---
+++ cw-1-AldanaAlRashidi-61.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+// Part 1
+var str = "Hello, playground"
+let name = "Aldana Saad AlRashidi"
+var hoppy1 = "Drawing"
+var hoppy2 = "Swimming"
+var age = 17
+var height = 155.3
+
+print ("Hello my name is")
+print(name)
+print ("My hoppies are")
+print (hoppy1)
+print (hoppy2)
+print ("My age is")
+print (age)
+print ("My height is")
+print (height)
+
+// Part 2
+let friendName = "Tahani"
+var friendHoppy1 = "Drawing"
+var friendHoppy2 = "Cooking"
+var friendAge = 20
+var friendHeight = 160.1
+
+let age_difference = friendAge - age
+
+name != friendName
+name == friendName
+
+hoppy1 == friendHoppy1
+hoppy1 != friendHoppy1
+
+hoppy2 == friendHoppy2
+hoppy2 != friendHoppy2
+
+age < friendAge
+age > friendAge
+
+height < friendHeight
+height > friendHeight
+
